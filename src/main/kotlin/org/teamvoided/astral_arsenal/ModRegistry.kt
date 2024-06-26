aae9ee554e4300681bc7a367d26c13fdac5770c4
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-06-26 15:38:03 -0400

    Creative tab

--- src/main/kotlin/org/teamvoided/astral_arsenal/ModRegistry.kt
+++ src/main/kotlin/org/teamvoided/astral_arsenal/ModRegistry.kt
@@ -1,22 +1,32 @@
 package org.teamvoided.astral_arsenal
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType
 import net.minecraft.block.AbstractBlock
 import net.minecraft.block.Block
 import net.minecraft.block.Blocks
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.sound.BlockSoundGroup
+import net.minecraft.text.Text
 import org.teamvoided.astral_arsenal.blocks.CosmicTableBlock
 
 object ModRegistry {
 
     val COSMIC_TABLE: Block =
-        registerBlock("cosmic_table", CosmicTableBlock(AbstractBlock.Settings.copy(Blocks.CRAFTING_TABLE).sounds(
-            BlockSoundGroup.LODESTONE)))
+        registerBlock(
+            "cosmic_table", CosmicTableBlock(
+                AbstractBlock.Settings.copy(Blocks.CRAFTING_TABLE).sounds(
+                    BlockSoundGroup.LODESTONE
+                )
+            )
+        )
 
     val COSMIC_TABLE_SCREEN_HANDLER_TYPE =
         registryScreenHandlerType(
@@ -26,7 +36,17 @@ object ModRegistry {
             }, CosmicTableData.PACKET_CODEC)
         )
 
-    fun init() {}
+    val TAB = RegistryKey.of(RegistryKeys.ITEM_GROUP, AstralArsenal.id("astral_arsenal"))
+
+    fun init() {
+        Registry.register(
+            Registries.ITEM_GROUP,
+            TAB,
+            FabricItemGroup.builder().name(Text.translatable("itemgroup.astral_arsenal.tab")).icon {
+                ItemStack(COSMIC_TABLE.asItem())
+            }.entries { _, entries -> entries.addItem(COSMIC_TABLE) }.build()
+        )
+    }
 
     private fun registerBlock(name: String, block: Block): Block {
         val instance = Registry.register(Registries.BLOCK, name, block)
