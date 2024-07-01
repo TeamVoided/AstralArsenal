bcd2b837bdb2308bd6a817fc191dc1e481f34422
wired-tomato <brokenfuse.self@gmail.com>
2024-07-01 05:21:37 -0400

    move recipes to pre-existing registered provider

--- src/main/kotlin/org/teamvoided/astralarsenal/data/gen/AstralRecipeProvider.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/data/gen/AstralRecipeProvider.kt
@@ -4,10 +4,13 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
 import net.minecraft.data.server.recipe.RecipeExporter
 import net.minecraft.data.server.recipe.ShapedRecipeJsonFactory
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonFactory
+import net.minecraft.item.Item
 import net.minecraft.item.ItemConvertible
 import net.minecraft.item.Items
 import net.minecraft.recipe.RecipeCategory
 import net.minecraft.registry.HolderLookup
+import net.minecraft.registry.Registries
 import org.teamvoided.astralarsenal.init.AstralItems
 import java.util.concurrent.CompletableFuture
 
@@ -31,6 +34,60 @@ class AstralRecipeProvider(
             .ingredient('C', Items.CRYING_OBSIDIAN)
             .criterion(hasItem(Items.END_CRYSTAL), conditionsFromItem(Items.END_CRYSTAL))
             .offerTo(this)
+
+        simpleShapeless(
+            AstralItems.AMERALD_BLEND,
+            RecipeCategory.REDSTONE, 1,
+            listOf(AstralItems.AMETHYST_DUST, AstralItems.AMETHYST_DUST)
+        ).offerTo(this)
+
+        simpleShapeless(
+            AstralItems.LAZULICA_BLEND,
+            RecipeCategory.REDSTONE, 1,
+            listOf(AstralItems.LAPIS_LAZULI_DUST, AstralItems.QUARTZ_DUST)
+        ).offerTo(this)
+
+        simpleShapeless(
+            AstralItems.CONCENTRATED_AMETHYST_BLEND,
+            RecipeCategory.REDSTONE, 1,
+            listOf(AstralItems.AMETHYST_DUST, AstralItems.AMETHYST_DUST, AstralItems.AMETHYST_DUST, AstralItems.AMETHYST_DUST)
+        ).offerTo(this)
+
+        simpleShapeless(
+            AstralItems.KOSMIK_GEM,
+            RecipeCategory.REDSTONE, 1,
+            listOf(AstralItems.LAZULICA, AstralItems.AMERALD, Items.DIAMOND)
+        ).offerTo(this)
+
+        simpleShapeless(
+            AstralItems.KOSMIK_GEM,
+            RecipeCategory.REDSTONE, 1,
+            listOf(AstralItems.LAZULICA, AstralItems.AMETHYST, Items.DIAMOND)
+        ).offerTo(this, AstralItems.KOSMIK_GEM.id.extendPath("_amethyst"))
+
+        simpleShapeless(
+            AstralItems.AMETHYST_DUST,
+            RecipeCategory.REDSTONE, 1,
+            listOf(Items.AMETHYST_SHARD)
+        ).offerTo(this)
+
+        simpleShapeless(
+            AstralItems.EMERALD_DUST,
+            RecipeCategory.REDSTONE, 1,
+            listOf(Items.EMERALD)
+        ).offerTo(this)
+
+        simpleShapeless(
+            AstralItems.LAPIS_LAZULI_DUST,
+            RecipeCategory.REDSTONE, 1,
+            listOf(Items.LAPIS_LAZULI)
+        ).offerTo(this)
+
+        simpleShapeless(
+            AstralItems.QUARTZ_DUST,
+            RecipeCategory.REDSTONE, 1,
+            listOf(Items.QUARTZ)
+        ).offerTo(this)
     }
 
     private fun shaped(
@@ -40,4 +97,21 @@ class AstralRecipeProvider(
     ): ShapedRecipeJsonFactory {
         return ShapedRecipeJsonFactory(category, result, count)
     }
+
+    private fun simpleShapeless(
+        result: ItemConvertible,
+        category: RecipeCategory = RecipeCategory.MISC,
+        count: Int = 1,
+        ingredients: List<ItemConvertible>
+    ): ShapelessRecipeJsonFactory {
+        val factory = ShapelessRecipeJsonFactory(category, result, count)
+        ingredients.forEach(factory::ingredient)
+        ingredients.toSet().forEach {
+            factory.criterion(hasItem(it), conditionsFromItem(it))
+        }
+
+        return factory
+    }
+
+    val Item.id get() = Registries.ITEM.getId(this)
 }
