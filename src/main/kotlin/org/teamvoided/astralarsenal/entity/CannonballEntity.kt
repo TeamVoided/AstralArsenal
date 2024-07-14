6d52aaf6061f8fb05a9a29e3146656289eba015c
Astratine <davebobsos910@gmail.com>
2024-07-14 12:29:12 +0100

    Cannonballing(unfinished)

--- src/main/kotlin/org/teamvoided/astralarsenal/entity/CannonballEntity.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/entity/CannonballEntity.kt
@@ -1,9 +1,13 @@
 package org.teamvoided.astralarsenal.entity
 
+import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.ProjectileDeflector
+import net.minecraft.entity.projectile.ProjectileEntity
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity
 import net.minecraft.item.Item
+import net.minecraft.util.random.RandomGenerator
 import net.minecraft.world.World
 import org.teamvoided.astralarsenal.init.AstralEntities
 import org.teamvoided.astralarsenal.init.AstralItems
@@ -20,5 +24,30 @@ class CannonballEntity : ThrownItemEntity {
 
     override fun getDefaultItem(): Item {
         return AstralItems.ASTRAL_GREATHAMMER
+    }
+
+        override fun method_59859(
+            projectileDeflector: ProjectileDeflector,
+            entity2: Entity?,
+            entity: Entity?,
+            bl: Boolean
+        ): Boolean {
+            if (!world.isClient) {
+                deflecting.deflect(this, entity2, this.random)
+                this.owner = entity
+                this.method_59525(entity2, bl)
+            }
+
+            return true
+        }
+    companion object {
+        val deflecting: ProjectileDeflector =
+            ProjectileDeflector { projectileEntity: ProjectileEntity, entity: Entity?, random: RandomGenerator? ->
+                if (entity != null) {
+                    val vec3d = entity.rotationVector.normalize().multiply(2.0)
+                    projectileEntity.velocity = vec3d
+                    projectileEntity.velocityDirty = true
+                }
+            }
     }
 }
--- src/main/kotlin/org/teamvoided/astralarsenal/item/AstralGreathammerItem.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/item/AstralGreathammerItem.kt
@@ -9,14 +9,15 @@ import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 import org.teamvoided.astralarsenal.entity.CannonballEntity
 import org.teamvoided.astralarsenal.init.AstralEntities
-
+// Please change the summoning functionality into a kosmogliph for cannonball
+//Incomplete, still needs to push the cannonball up and in front a small ammount
 class AstralGreathammerItem(settings: Item.Settings) : SwordItem(ToolMaterials.NETHERITE, settings) {
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         if (!world.isClient){
             val snowballEntity = CannonballEntity(world, user)
-            snowballEntity.setProperties(user, user.pitch, user.yaw, 0.0f, 1.5f, 1.0f)
+            snowballEntity.setProperties(user, user.pitch, user.yaw, 0.0f, 0.0f, 0.0f)
             world.spawnEntity(snowballEntity)
-
+            user.itemCooldownManager.set(this, 100)
         }
         return super.use(world, user, hand)
     }
