570692482041a06304062c69196bf67990d62437
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-07-18 17:25:08 -0400

    Add state to jump handler to prevent spamming

--- src/client/kotlin/org/teamvoided/astralarsenal/AstralArsenalClient.kt
+++ src/client/kotlin/org/teamvoided/astralarsenal/AstralArsenalClient.kt
@@ -1,11 +1,10 @@
 package org.teamvoided.astralarsenal
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.minecraft.client.MinecraftClient
+import org.teamvoided.astralarsenal.handlers.AstralJumpKeyHandler
 import org.teamvoided.astralarsenal.init.AstralEntitiesClient
 import org.teamvoided.astralarsenal.init.AstralHandledScreens
-import org.teamvoided.astralarsenal.networking.JumpKosmogliphPayload
 
 @Suppress("unused")
 object AstralArsenalClient {
@@ -15,10 +14,6 @@ object AstralArsenalClient {
         AstralEntitiesClient.clientInit()
         MinecraftClient.getInstance()
 
-        ClientTickEvents.END_CLIENT_TICK.register { client ->
-            if (client.options.jumpKey.isPressed) {
-                ClientPlayNetworking.send(JumpKosmogliphPayload)
-            }
-        }
+        ClientTickEvents.END_CLIENT_TICK.register(AstralJumpKeyHandler())
     }
 }
