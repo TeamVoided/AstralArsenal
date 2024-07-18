6dd79be3d2cf6a122c73b6a6be88c39278e91b32
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-07-18 17:49:11 -0400

    Add boost when double jumping

--- src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/JumpKosmogliph.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/JumpKosmogliph.kt
@@ -18,12 +18,16 @@ class JumpKosmogliph(id: Identifier) : SimpleKosmogliph(id, {
     val item = it.item
     item is ArmorItem && item.armorSlot == ArmorItem.ArmorSlot.BOOTS
 }) {
+    // Change this to change how much boost is given when double-jumping.
+    val JUMP_FORWARD_BOOST = 0.3
+
     fun handleJump(stack: ItemStack, player: PlayerEntity) {
         val data = stack.get(AstralItemComponents.JUMP_DATA) ?: throw IllegalStateException("Erm, how the fuck did you manage this")
         val world = player.world
 
         if(data.uses > 0 && !player.isOnGround && data.lastJump >= 10) {
-            player.setVelocity(player.velocity.x,0.5, player.velocity.z)
+            val boost = player.rotationVector.multiply(1.0, 0.0, 1.0).normalize().multiply(JUMP_FORWARD_BOOST)
+            player.setVelocity(player.velocity.x + boost.x,0.5, player.velocity.z + boost.z)
             player.velocityModified = true
             world.playSound(
                 null,
@@ -44,7 +48,7 @@ class JumpKosmogliph(id: Identifier) : SimpleKosmogliph(id, {
         var lastJump = data.lastJump
         if (uses >= 3) return
         var cooldown = data.cooldown
-        if(entity.isOnGround) cooldown--;
+        if(entity.isOnGround) cooldown--
 
         if (cooldown <= 0) {
             uses++
