9af0ee256d826485e9c486264adcd424ee06fcf3
theendercore <theendercore@gmail.com>
2024-07-20 21:12:42 +0300

    - fixed AstralEntities

--- src/main/kotlin/org/teamvoided/astralarsenal/init/AstralEntities.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/init/AstralEntities.kt
@@ -1,8 +1,6 @@
 package org.teamvoided.astralarsenal.init
 
-import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
 import net.minecraft.entity.Entity
-import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.SpawnGroup
 import net.minecraft.registry.Registries
@@ -11,64 +9,39 @@ import org.teamvoided.astralarsenal.AstralArsenal.id
 import org.teamvoided.astralarsenal.entity.*
 
 object AstralEntities {
+
+    fun init() = Unit
+
     val CANNONBALL_ENTITY = register(
-    "cannonball",
-    FabricEntityTypeBuilder.create(
-        SpawnGroup.MISC,
-        EntityType.EntityFactory(:: CannonballEntity)
+        "cannonball",
+        EntityType.Builder.create(EntityType.EntityFactory(::CannonballEntity), SpawnGroup.MISC)
+            .setDimensions(0.5f, 0.5f).maxTrackingRange(4).build()
     )
-        .dimensions(EntityDimensions.fixed(0.5f, 0.5f))
-        .trackRangeBlocks(4).build()
-)
     val MORTER_ENTITY = register(
         "morter",
-        FabricEntityTypeBuilder.create(
-            SpawnGroup.MISC,
-            EntityType.EntityFactory(::MorterEntity)
-        )
-            .dimensions(EntityDimensions.fixed(0.5f, 0.5f))
-            .trackRangeBlocks(4).build()
+        EntityType.Builder.create(EntityType.EntityFactory(::MorterEntity), SpawnGroup.MISC)
+            .setDimensions(0.5f, 0.5f).maxTrackingRange(4).build()
     )
     val BEAM_OF_LIGHT = register(
         "beam_of_light",
-        FabricEntityTypeBuilder.create(
-            SpawnGroup.MISC,
-            EntityType.EntityFactory(::BeamOfLightEntity)
-        )
-            .dimensions(EntityDimensions.fixed(0.5f, 0.5f))
-            .trackRangeBlocks(4).build()
+        EntityType.Builder.create(EntityType.EntityFactory(::BeamOfLightEntity), SpawnGroup.MISC)
+            .setDimensions(0.5f, 0.5f).maxTrackingRange(4).build()
     )
     val SLASH_ENTITY = register(
         "slash_entity",
-        FabricEntityTypeBuilder.create(
-            SpawnGroup.MISC,
-            EntityType.EntityFactory(::SlashEntity)
-        )
-            .dimensions(EntityDimensions.fixed(0.5f, 0.5f))
-            .trackRangeBlocks(4).build()
+        EntityType.Builder.create(EntityType.EntityFactory(::SlashEntity), SpawnGroup.MISC)
+            .setDimensions(0.5f, 0.5f).maxTrackingRange(4).build()
     )
     val BOOM_SHOOTER_ENTITY = register(
         "boom_shooter_entity",
-        FabricEntityTypeBuilder.create(
-            SpawnGroup.MISC,
-            EntityType.EntityFactory(::BoomShooterEntity)
-        )
-            .dimensions(EntityDimensions.fixed(0.5f, 0.5f))
-            .trackRangeBlocks(4).build()
+        EntityType.Builder.create(EntityType.EntityFactory(::BoomShooterEntity), SpawnGroup.MISC)
+            .setDimensions(0.5f, 0.5f).maxTrackingRange(4).build()
     )
     val BOOM_ENTITY = register(
         "boom_entity",
-        FabricEntityTypeBuilder.create(
-            SpawnGroup.MISC,
-            EntityType.EntityFactory(::BoomEntity)
-        )
-            .dimensions(EntityDimensions.fixed(0.5f, 0.5f))
-            .trackRangeBlocks(4).build()
+        EntityType.Builder.create(EntityType.EntityFactory(::BoomEntity), SpawnGroup.MISC)
+            .setDimensions(0.5f, 0.5f).maxTrackingRange(4).build()
     )
-
-    fun serverInit() {
-
-    }
 
     private fun <T : Entity> register(path: String, entry: EntityType<T>): EntityType<T> {
         return Registry.register(Registries.ENTITY_TYPE, id(path), entry)
