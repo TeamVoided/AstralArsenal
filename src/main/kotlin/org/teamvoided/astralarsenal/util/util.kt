3d39cca9391cb7f07c5876f497014095ef3f1b0c
theendercore <theendercore@gmail.com>
2024-07-20 21:12:54 +0300

    - register utils

--- src/main/kotlin/org/teamvoided/astralarsenal/util/util.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/util/util.kt
@@ -1,6 +1,7 @@
 package org.teamvoided.astralarsenal.util
 
 import net.minecraft.item.ItemStack
+import net.minecraft.registry.Holder
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.tag.TagKey
@@ -8,7 +9,10 @@ import net.minecraft.util.Identifier
 import org.teamvoided.astralarsenal.init.AstralItemComponents.KOSMOGLIPHS
 import org.teamvoided.astralarsenal.item.components.KosmogliphsComponent
 
-fun <T, R: Registry<T>> RegistryKey<R>.tag(id: Identifier) = TagKey.of(this, id)
+fun <T, R : Registry<T>> RegistryKey<R>.tag(id: Identifier) = TagKey.of(this, id)
+
+fun <T> Registry<T>.registerHolder(id: Identifier, entry:T): Holder.Reference<T> = Registry.registerHolder(this, id, entry)
+fun <T> Registry<T>.register(id: Identifier, entry:T): T = Registry.register(this, id, entry)
 
 fun getKosmogliphsOnStack(stack: ItemStack): KosmogliphsComponent {
     val component = stack.components.get(KOSMOGLIPHS)
