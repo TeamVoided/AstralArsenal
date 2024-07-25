e6d6d55b7b3da38b3749827e5fa485dcf23adc51
wired-tomato <brokenfuse.self@gmail.com>
2024-07-25 09:24:05 -0400

    dont spawn 500 threads

--- src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/ranged/beams/FlameThrowerKosmogliph.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/ranged/beams/FlameThrowerKosmogliph.kt
@@ -1,10 +1,10 @@
 package org.teamvoided.astralarsenal.item.kosmogliph.ranged.beams
 
+import kotlinx.coroutines.delay
 import net.minecraft.entity.Entity
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.damage.DamageTypes
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.SwordItem
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundCategory
@@ -12,86 +12,79 @@ import net.minecraft.sound.SoundEvents
 import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.Box
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 import org.joml.Math.lerp
 import org.teamvoided.astralarsenal.coroutine.mcCoroutineTask
-import org.teamvoided.astralarsenal.coroutine.ticks
-import org.teamvoided.astralarsenal.entity.BoomEntity
-import org.teamvoided.astralarsenal.entity.CannonballEntity
 import org.teamvoided.astralarsenal.init.AstralDamageTypes
-import org.teamvoided.astralarsenal.init.AstralDamageTypes.customDamage
-import org.teamvoided.astralarsenal.init.AstralSounds
-import org.teamvoided.astralarsenal.item.AstralGreathammerItem
 import org.teamvoided.astralarsenal.item.RailgunItem
 import org.teamvoided.astralarsenal.item.kosmogliph.SimpleKosmogliph
-import kotlin.math.pow
 import kotlin.math.roundToInt
-import kotlin.math.sqrt
 import kotlin.time.Duration.Companion.seconds
 
 class FlameThrowerKosmogliph(id: Identifier) :
-    SimpleKosmogliph(id, { it.item is RailgunItem }){
+    SimpleKosmogliph(id, { it.item is RailgunItem }) {
     override fun onUse(world: World, player: PlayerEntity, hand: Hand) {
-        for(i in 0..500) {
-            mcCoroutineTask(delay = (i * 0.005).seconds) {
+        mcCoroutineTask {
+            val stack = player.getStackInHand(hand)
+            for (i in 0..250) {
+                if (stack != player.getStackInHand(hand)) return@mcCoroutineTask
+
                 val result = player.raycast(10.0, 1f, false)
-                val distance = sqrt(
-                    sqrt((player.eyePos.x - result.pos.x).pow(2) + (player.eyePos.z - result.pos.z).pow(2)).pow(2) + ((player.eyePos.y- 0.5) - result.pos.y).pow(
-                        2
-                    )
-                )
+                val distance = player.eyePos.distanceTo(result.pos)
                 val entities = mutableListOf<Entity>()
-                val interval = (distance.times(2))
-                for (j in 0..interval.roundToInt()) {
-                    entities.addAll(
-                        world.getOtherEntities(
-                            player, Box(
-                                (lerp(player.eyePos.x, result.pos.x, j / interval)) + 0.5,
-                                (lerp(player.eyePos.y- 0.5, result.pos.y, j / interval)) + 0.5,
-                                (lerp(player.eyePos.z, result.pos.z, j / interval)) + 0.5,
-                                (lerp(player.eyePos.x, result.pos.x, j / interval)) - 0.5,
-                                (lerp(player.eyePos.y- 0.5, result.pos.y, j / interval)) - 0.5,
-                                (lerp(player.eyePos.z, result.pos.z, j / interval)) - 0.5
-                            )
+                entities.addAll(
+                    world.getOtherEntities(
+                        player, Box(
+                            player.eyePos.subtract(Vec3d(0.0, 0.5, 0.0)),
+                            result.pos
                         )
                     )
-                    if(i % 20 == 0){
-                        if (!player.world.isClient) {
-                         val serverWorld = player.world as ServerWorld
-                            serverWorld.spawnParticles(
-                                ParticleTypes.FLAME,
-                                (lerp(player.eyePos.x, result.pos.x, j / interval)),
-                                (lerp(player.eyePos.y - 0.5, result.pos.y, j / interval)),
-                                (lerp(player.eyePos.z, result.pos.z, j / interval)),
-                                1,
-                                0.2,
-                                0.2,
-                                0.2,
-                                0.0
-                         )
-                        }
+                )
+
+                val interval = (distance * 2).roundToInt()
+                if (!world.isClient && world is ServerWorld) {
+                    for (j in 0..interval) {
+                        world.spawnParticles(
+                            ParticleTypes.FLAME,
+                            (lerp(player.eyePos.x, result.pos.x, j / interval.toDouble())),
+                            (lerp(player.eyePos.y - 0.5, result.pos.y, j / interval.toDouble())),
+                            (lerp(player.eyePos.z, result.pos.z, j / interval.toDouble())),
+                            1,
+                            0.2,
+                            0.2,
+                            0.2,
+                            0.0
+                        )
+
+                        world.playSound(
+                            null,
+                            player.x,
+                            player.y,
+                            player.z,
+                            SoundEvents.BLOCK_FIRE_AMBIENT,
+                            SoundCategory.PLAYERS,
+                            1.0F,
+                            1.0f
+                        )
                     }
                 }
-                for (entity in entities) {
+
+                entities.forEach { entity ->
                     entity.damage(
                         DamageSource(
                             AstralDamageTypes.getHolder(world.registryManager, DamageTypes.IN_FIRE),
                             player,
                             player
-                        ), 3f)
+                        ), 3f
+                    )
                     entity.setOnFireFor(200)
                 }
-                if(i % 20 == 0){
-                world.playSound(null,
-                    player.x,
-                    player.y,
-                    player.z,
-                    SoundEvents.BLOCK_FIRE_AMBIENT,
-                    SoundCategory.PLAYERS,
-                    1.0F,
-                    1.0f)}
+
+                delay((0.08).seconds)
             }
         }
+
         if (!player.isCreative) {
             player.itemCooldownManager.set(player.getStackInHand(hand).item, 400)
         }
