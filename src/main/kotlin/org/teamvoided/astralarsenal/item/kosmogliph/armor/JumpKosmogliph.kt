8c1cb906ebb097db4f5d54461256e66b3838c1eb
theendercore <theendercore@gmail.com>
2024-08-05 18:58:44 +0300

    - jump aesthetic rework - you can no longer use it while in a vehicle

--- src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/armor/JumpKosmogliph.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/armor/JumpKosmogliph.kt
@@ -1,7 +1,6 @@
 package org.teamvoided.astralarsenal.item.kosmogliph.armor
 
 import net.minecraft.enchantment.Enchantment
-import net.minecraft.enchantment.Enchantments
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffects
@@ -17,9 +16,7 @@ import net.minecraft.util.Identifier
 import net.minecraft.util.dynamic.Codecs
 import net.minecraft.world.World
 import org.teamvoided.astralarsenal.init.AstralItemComponents
-import org.teamvoided.astralarsenal.init.AstralSounds
 import org.teamvoided.astralarsenal.item.kosmogliph.SimpleKosmogliph
-import java.lang.Math.random
 
 class JumpKosmogliph(id: Identifier) : SimpleKosmogliph(id, {
     val item = it.item
@@ -29,36 +26,45 @@ class JumpKosmogliph(id: Identifier) : SimpleKosmogliph(id, {
     val JUMP_FORWARD_BOOST = 0.3
 
     fun handleJump(stack: ItemStack, player: PlayerEntity) {
-        val data = stack.get(AstralItemComponents.JUMP_DATA) ?: throw IllegalStateException("Erm, how the fuck did you manage this")
+        val data = stack.get(AstralItemComponents.JUMP_DATA)
+            ?: throw IllegalStateException("Erm, how the fuck did you manage this")
         val world = player.world
 
-        if(data.uses > 0 && !player.isOnGround) {
-            var boost = player.rotationVector.multiply(0.0,0.0,0.0)
-            if(player.velocity.x > 0.0 || player.velocity.z > 0.0){boost = player.rotationVector.multiply(1.0, 0.0, 1.0).normalize().multiply(JUMP_FORWARD_BOOST)}
-            player.setVelocity(player.velocity.x + boost.x,0.5, player.velocity.z + boost.z)
+        if (player.vehicle != null) return
+
+        if (data.uses > 0 && !player.isOnGround) {
+            var boost = player.rotationVector.multiply(0.0, 0.0, 0.0)
+            if (player.velocity.x > 0.0 || player.velocity.z > 0.0) {
+                boost = player.rotationVector.multiply(1.0, 0.0, 1.0).normalize().multiply(JUMP_FORWARD_BOOST)
+            }
+            player.setVelocity(player.velocity.x + boost.x, 0.5, player.velocity.z + boost.z)
             player.velocityModified = true
-            player.hungerManager.add(0,-0.1f)
+            player.hungerManager.add(0, -0.1f)
             world.playSound(
                 null,
                 player.x,
                 player.y,
                 player.z,
-                SoundEvents.BLOCK_ROOTED_DIRT_PLACE,
+                SoundEvents.ENTITY_BREEZE_JUMP,
                 SoundCategory.PLAYERS,
                 1.0F,
-                1.0F)
-            if(!world.isClient){
+                1.0F
+            )
+            if (!world.isClient) {
                 val serverWorld = world as ServerWorld
-                serverWorld.spawnParticles(
-                    ParticleTypes.SPIT,
-                    player.x,
-                    player.y - 1,
-                    player.z,
-                    20,
-                    random().minus(0.5).times(2),
-                    0.0,
-                    random().minus(0.5).times(2),
-                    0.0)
+                repeat(20) {
+                    serverWorld.spawnParticles(
+                        ParticleTypes.SPIT,
+                        player.x,
+                        player.y - 0.1,
+                        player.z,
+                        1,
+                        (world.random.nextDouble() - 0.5) * 1.7,
+                        0.0,
+                        (world.random.nextDouble() - 0.5) * 1.7,
+                        0.0
+                    )
+                }
             }
             stack.set(AstralItemComponents.JUMP_DATA, Data(data.uses - 1, data.cooldown, 0, data.maxUses - 1))
         }
@@ -78,13 +84,11 @@ class JumpKosmogliph(id: Identifier) : SimpleKosmogliph(id, {
                 if (entity.hungerManager.foodLevel > 6) {
                     if (uses < maxUses) cooldown--
                 }
-            } else {
-                cooldown--
-            }
+            } else { cooldown-- }
 
             if (cooldown <= 0) {
                 uses++
-                val x: Float = (uses * 0.5).toFloat()
+                val x: Float = (uses * 2.0).toFloat()
                 var time = 20
                 if (entity is LivingEntity) {
                     val y = entity.statusEffects.filter { it.effectType == StatusEffects.SLOWNESS }
@@ -100,9 +104,9 @@ class JumpKosmogliph(id: Identifier) : SimpleKosmogliph(id, {
                     entity.x,
                     entity.y,
                     entity.z,
-                    AstralSounds.CHARGE,
+                    SoundEvents.BLOCK_END_PORTAL_FRAME_FILL,
                     SoundCategory.PLAYERS,
-                    1.0F,
+                    0.6F,
                     x
                 )
             }
@@ -127,6 +131,7 @@ class JumpKosmogliph(id: Identifier) : SimpleKosmogliph(id, {
                 )
         }
     }
+
     override fun disallowedEnchantment(): List<RegistryKey<Enchantment>> {
         return listOf()
     }
