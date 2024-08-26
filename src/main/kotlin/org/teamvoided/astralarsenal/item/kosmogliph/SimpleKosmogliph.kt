96d33b9a6361143c8fdef3b9f0dc4d5c9997705d
theendercore <theendercore@gmail.com>
2024-08-26 21:26:14 +0300

    - tag constructor for SimpleKosmogliph

--- src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/SimpleKosmogliph.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/item/kosmogliph/SimpleKosmogliph.kt
@@ -4,6 +4,7 @@ import arrow.core.Predicate
 import net.minecraft.client.item.TooltipConfig
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.registry.tag.TagKey
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 
@@ -11,6 +12,7 @@ open class SimpleKosmogliph(
     val id: Identifier,
     val applicationPredicate: Predicate<ItemStack>
 ) : Kosmogliph {
+    constructor(id: Identifier, tag: TagKey<Item>) : this(id, { it.isIn(tag) })
     override fun modifyItemTooltip(
         stack: ItemStack,
         ctx: Item.TooltipContext,
