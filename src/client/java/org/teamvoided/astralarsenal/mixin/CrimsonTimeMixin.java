9dbc69eee52124067b3ca7ca5009bdeeb55a67b5
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-09-12 09:55:36 -0400

    Don't show the crimson crosshair if the target is invisible

--- src/client/java/org/teamvoided/astralarsenal/mixin/CrimsonTimeMixin.java
+++ src/client/java/org/teamvoided/astralarsenal/mixin/CrimsonTimeMixin.java
@@ -4,6 +4,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -41,7 +43,8 @@ public class CrimsonTimeMixin {
 
     if(ticks > 0) {
       ticks--;
-      if(instance.targetedEntity == null)
+
+      if(instance.targetedEntity == null && target instanceof LivingEntity living && !living.hasStatusEffect(StatusEffects.INVISIBILITY))
         AstralHudRendering.INSTANCE.setCrimsonCrosshair(true);
     } else {
       target = null;
