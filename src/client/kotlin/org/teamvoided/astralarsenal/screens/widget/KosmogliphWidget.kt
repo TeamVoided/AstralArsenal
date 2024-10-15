eec66f1443ff43a118b07bc008ef9f9965d1a1a6
theendercore <theendercore@gmail.com>
2024-10-15 15:32:36 +0300

    - applied kosmogliphs now have a custom background

--- src/client/kotlin/org/teamvoided/astralarsenal/screens/widget/KosmogliphWidget.kt
+++ src/client/kotlin/org/teamvoided/astralarsenal/screens/widget/KosmogliphWidget.kt
@@ -6,9 +6,11 @@ import net.minecraft.client.gui.screen.narration.NarrationPart
 import net.minecraft.client.gui.tooltip.Tooltip
 import net.minecraft.client.gui.widget.ClickableWidget
 import net.minecraft.text.Text
+import net.minecraft.util.Identifier
 import org.teamvoided.astralarsenal.AstralArsenal.id
 import org.teamvoided.astralarsenal.kosmogliph.Kosmogliph
 import org.teamvoided.astralarsenal.menu.CosmicTableMenu
+import org.teamvoided.astralarsenal.util.getKosmogliphsOnStack
 
 abstract class KosmogliphWidget(
     x: Int, y: Int,
@@ -17,33 +19,34 @@ abstract class KosmogliphWidget(
     val kosmogliph: Kosmogliph,
     val handler: CosmicTableMenu
 ) : ClickableWidget(x, y, width, height, message) {
-    var compatible = !handler.isIncompatible(kosmogliph, handler.getSlot(0).stack)
-    val kosmogliphTexture = kosmogliph.id().withPrefix("kosmogliph/")
-
+    val kosmogliphTexture: Identifier = kosmogliph.id().withPrefix("kosmogliph/")
+    val BTN_TEXTURE: Identifier
 
     init {
         tooltip = Tooltip.create(handler.createKosmogliphTooltip(kosmogliph))
+        BTN_TEXTURE = getTexture()
     }
 
-    override fun drawWidget(
-        graphics: GuiGraphics,
-        mouseX: Int,
-        mouseY: Int,
-        delta: Float
-    ) {
-        val stack = graphics.matrices
-        stack.push()
-        graphics.drawGuiTexture(if (compatible) COMPATIBLE_TEXTURE else INCOMPATIBLE_TEXTURE, x, y, width, height)
+    override fun drawWidget(graphics: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) {
+        graphics.matrices.push()
+        graphics.drawGuiTexture(BTN_TEXTURE, x, y, width, height)
         graphics.drawGuiTexture(kosmogliphTexture, x + 2, y + 2, (width / 1.2).toInt(), (height / 1.2).toInt())
-        stack.pop()
+        graphics.matrices.pop()
     }
 
     override fun updateNarration(builder: NarrationMessageBuilder) {
         builder.put(NarrationPart.HINT, Text.translatable(kosmogliph.id().toTranslationKey("kosmogliph.name")))
     }
 
+    private fun getTexture(): Identifier {
+        return if (handler.isIncompatible(kosmogliph, handler.getSlot(0).stack)) INCOMPATIBLE_TEXTURE
+        else if (getKosmogliphsOnStack(handler.getSlot(0).stack).contains(kosmogliph)) SELECTED_TEXTURE
+        else COMPATIBLE_TEXTURE
+    }
+
     companion object {
         val INCOMPATIBLE_TEXTURE = id("widget/kosmogliph/incompatible")
+        val SELECTED_TEXTURE = id("widget/kosmogliph/selected")
         val COMPATIBLE_TEXTURE = id("widget/kosmogliph/compatible")
 
         const val SIZE = 22
