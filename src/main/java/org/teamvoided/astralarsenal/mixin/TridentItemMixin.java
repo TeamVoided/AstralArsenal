7a427284ddba73bb0d0221fa4c86cd222f7c90dc
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-10-20 17:11:41 -0400

    Trident sound condition mixin

--- src/main/java/org/teamvoided/astralarsenal/mixin/TridentItemMixin.java
+++ src/main/java/org/teamvoided/astralarsenal/mixin/TridentItemMixin.java
@@ -1,16 +1,23 @@
 package org.teamvoided.astralarsenal.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TridentItem;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.teamvoided.astralarsenal.components.KosmogliphsComponent;
 import org.teamvoided.astralarsenal.init.AstralItemComponents;
 import org.teamvoided.astralarsenal.init.AstralKosmogliphs;
 import org.teamvoided.astralarsenal.kosmogliph.ranged.trident.AstralRainKosmogliph;
@@ -21,6 +28,7 @@ import static org.teamvoided.astralarsenal.util.UtilKt.getKosmogliphsOnStack;
 
 @Mixin(TridentItem.class)
 public class TridentItemMixin {
+
     @Redirect(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
     private boolean isWetOrUsingKosmogliph(PlayerEntity instance, @Local(argsOnly = true) ItemStack stack) {
         var charges = Objects.requireNonNull(stack.get(AstralItemComponents.ASTRAL_RAIN_DATA)).getCharges();
@@ -29,10 +37,12 @@ public class TridentItemMixin {
             charges = 0;
         }
         if (instance.isTouchingWaterOrRain() || (hasAstralRain && charges > 0)) {
-            if(!instance.isTouchingWaterOrRain()) stack.set(AstralItemComponents.ASTRAL_RAIN_DATA, new AstralRainKosmogliph.Data(charges - 1));
-            if(hasAstralRain){
+            if (!instance.isTouchingWaterOrRain()) {
+                stack.set(AstralItemComponents.ASTRAL_RAIN_DATA, new AstralRainKosmogliph.Data(charges - 1));
+            }
+            if (hasAstralRain) {
                 instance.playSound(SoundEvents.BLOCK_TRIAL_SPAWNER_SPAWN_ITEM_BEGIN, 2.0f, 0.5f);
-                if(instance.getWorld() instanceof ServerWorld){
+                if (instance.getWorld() instanceof ServerWorld) {
                     ((ServerWorld) instance.getWorld()).spawnParticles(
                             ParticleTypes.GLOW,
                             instance.getX() + (instance.getWorld().random.nextDouble() - 0.5) * 1.7,
@@ -43,12 +53,24 @@ public class TridentItemMixin {
                             instance.getVelocity().y,
                             instance.getVelocity().z,
                             -0.2
-                            );
+                    );
                 }
             }
             return true;
-        } else return false;
+        }
+        else {
+            return false;
+        }
     }
+
+    @WrapOperation(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSoundFromEntity(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V", ordinal = 1))
+    private void canPlaySoundFromEntity(World instance, PlayerEntity except, Entity entity, SoundEvent sound, SoundCategory category, float volume, float pitch, Operation<Void> original, ItemStack stack) {
+        KosmogliphsComponent kosmogliphs = getKosmogliphsOnStack(stack);
+        if (!kosmogliphs.contains(AstralKosmogliphs.ASTRAL_RAIN)) {
+            original.call(instance, except, entity, sound, category, volume, pitch);
+        }
+    }
+
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
     private boolean isWetOrUsingKosmogliph2(PlayerEntity instance, @Local(argsOnly = true) Hand hand) {
         ItemStack stack = instance.getStackInHand(hand);
@@ -57,6 +79,6 @@ public class TridentItemMixin {
         if (stack.get(AstralItemComponents.ASTRAL_RAIN_DATA) == null) {
             charges = 0;
         }
-        return instance.isTouchingWaterOrRain() || ( hasAstralRain && charges > 0);
+        return instance.isTouchingWaterOrRain() || (hasAstralRain && charges > 0);
     }
 }
