1abce88594f7fd9aed8f557a6c7fcad5c1bd3f98
TheEnderCore <theendercore@gmail.com>
2025-01-24 14:35:02 +0200

    - "fun default()" -> "val DEFAULT"

--- src/main/kotlin/org/teamvoided/astralarsenal/components/StorageComponents.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/components/StorageComponents.kt
@@ -17,7 +17,7 @@ import java.util.*
 data class JumpData(val uses: Int, val cooldown: Int, val lastJump: Int, val maxUses: Int) :
     SimpleStorageComponent {
     companion object {
-        fun default(): JumpData = JumpData(0, 0, 0, 0)
+        val DEFAULT: JumpData = JumpData(0, 0, 0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> JumpData(list[0], list[1], list.getOrNull(2) ?: 0, list[3]) },
             { data -> listOf(data.uses, data.cooldown, data.lastJump, data.maxUses) }
@@ -27,7 +27,7 @@ data class JumpData(val uses: Int, val cooldown: Int, val lastJump: Int, val max
 
 data class DashData(val uses: Int, val cooldown: Int): SimpleStorageComponent {
     companion object {
-        fun default(): DashData = DashData(0, 0)
+        val DEFAULT: DashData = DashData(0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> DashData(list[0], list[1]) },
             { data -> listOf(data.uses, data.cooldown) }
@@ -37,7 +37,7 @@ data class DashData(val uses: Int, val cooldown: Int): SimpleStorageComponent {
 
 data class DodgeData(val uses: Int, val cooldown: Int) : SimpleStorageComponent {
     companion object {
-        fun default(): DodgeData = DodgeData(0, 0)
+        val DEFAULT: DodgeData = DodgeData(0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> DodgeData(list[0], list[1]) },
             { data -> listOf(data.uses, data.cooldown) }
@@ -47,7 +47,7 @@ data class DodgeData(val uses: Int, val cooldown: Int) : SimpleStorageComponent 
 
 data class SlamData(val lastFallDistance: Float, val slamming: Boolean) : SimpleStorageComponent {
     companion object {
-        fun default(): SlamData = SlamData(0f, false)
+        val DEFAULT: SlamData = SlamData(0f, false)
         val CODEC = RecordCodecBuilder.create<SlamData> { builder ->
             builder.group(
                 Codec.FLOAT.fieldOf("lastFallDistance").forGetter { it.lastFallDistance },
@@ -59,7 +59,7 @@ data class SlamData(val lastFallDistance: Float, val slamming: Boolean) : Simple
 
 data class AstralStrikeData(val hitTimes: Int) : SimpleStorageComponent {
     companion object {
-        fun default(): AstralStrikeData = AstralStrikeData(0)
+        val DEFAULT: AstralStrikeData = AstralStrikeData(0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> AstralStrikeData(list[0]) },
             { data -> listOf(data.hitTimes) }
@@ -69,7 +69,7 @@ data class AstralStrikeData(val hitTimes: Int) : SimpleStorageComponent {
 
 data class MinigunData(val uses: Int, val cooldown: Int) : SimpleStorageComponent {
     companion object {
-        fun default(): MinigunData = MinigunData(0, 0)
+        val DEFAULT: MinigunData = MinigunData(0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> MinigunData(list[0], list[1]) },
             { data -> listOf(data.uses, data.cooldown) }
@@ -79,7 +79,7 @@ data class MinigunData(val uses: Int, val cooldown: Int) : SimpleStorageComponen
 
 data class NailCannonDataV1(val uses: Int, val beingUsed: Int) : SimpleStorageComponent {
     companion object {
-        fun default(): NailCannonDataV1 = NailCannonDataV1(0, 0)
+        val DEFAULT: NailCannonDataV1 = NailCannonDataV1(0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> NailCannonDataV1(list[0], list[1]) },
             { data -> listOf(data.uses, data.beingUsed) }
@@ -89,7 +89,7 @@ data class NailCannonDataV1(val uses: Int, val beingUsed: Int) : SimpleStorageCo
 
 data class NailCannonCooldownData(val cooldown: Int, val fireCooldown: Int) : SimpleStorageComponent {
     companion object {
-        fun default(): NailCannonCooldownData = NailCannonCooldownData(0, 0)
+        val DEFAULT: NailCannonCooldownData = NailCannonCooldownData(0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> NailCannonCooldownData(list[0], list[1]) },
             { data -> listOf(data.cooldown, data.fireCooldown) }
@@ -99,7 +99,7 @@ data class NailCannonCooldownData(val cooldown: Int, val fireCooldown: Int) : Si
 
 data class CometLauncherData(val uses: Int, val cooldown: Int) : SimpleStorageComponent {
     companion object {
-        fun default(): CometLauncherData = CometLauncherData(0, 0)
+        val DEFAULT: CometLauncherData = CometLauncherData(0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> CometLauncherData(list[0], list[1]) },
             { data -> listOf(data.uses, data.cooldown) }
@@ -109,7 +109,7 @@ data class CometLauncherData(val uses: Int, val cooldown: Int) : SimpleStorageCo
 
 data class GrappleData(val jumps: Int, val timer: Int, val negateFallDamage: Boolean) : SimpleStorageComponent {
     companion object {
-        fun default(): GrappleData = GrappleData(0, 0, false)
+        val DEFAULT: GrappleData = GrappleData(0, 0, false)
         val CODEC = RecordCodecBuilder.create<GrappleData> { builder ->
             builder.group(
                 Codec.INT.fieldOf("jumps").forGetter { it.jumps },
@@ -123,7 +123,7 @@ data class GrappleData(val jumps: Int, val timer: Int, val negateFallDamage: Boo
 data class AlchemistData(val contents: Optional<PotionContentsComponent>, val charges: Int) :
     SimpleStorageComponent {
     companion object {
-        fun default(): AlchemistData = AlchemistData(Optional.empty(), 0)
+        val DEFAULT: AlchemistData = AlchemistData(Optional.empty(), 0)
         val CODEC = RecordCodecBuilder.create<AlchemistData> { builder ->
             builder.group(
                 PotionContentsComponent.CODEC.lenientOptionalFieldOf("contents").forGetter { it.contents },
@@ -135,7 +135,7 @@ data class AlchemistData(val contents: Optional<PotionContentsComponent>, val ch
 
 data class PulveriserData(val ticks: Int, val slamming: Boolean) : SimpleStorageComponent {
     companion object {
-        fun default(): PulveriserData = PulveriserData(0, false)
+        val DEFAULT: PulveriserData = PulveriserData(0, false)
         val CODEC = RecordCodecBuilder.create<PulveriserData> { builder ->
             builder.group(
                 Codec.INT.fieldOf("ticks").forGetter { it.ticks },
@@ -147,7 +147,7 @@ data class PulveriserData(val ticks: Int, val slamming: Boolean) : SimpleStorage
 
 data class AstralRainData(val charges: Int) : SimpleStorageComponent {
     companion object {
-        fun default(): AstralRainData = AstralRainData(0)
+        val DEFAULT: AstralRainData = AstralRainData(0)
         val CODEC = RecordCodecBuilder.create<AstralRainData> { builder ->
             builder.group(Codec.INT.fieldOf("ticks").forGetter { it.charges })
                 .apply(builder, ::AstralRainData)
@@ -157,7 +157,7 @@ data class AstralRainData(val charges: Int) : SimpleStorageComponent {
 
 data class SnipeDataV1(val ticks: Int, val loaded: Boolean) : SimpleStorageComponent {
     companion object {
-        fun default(): SnipeDataV1 = SnipeDataV1(0, false)
+        val DEFAULT: SnipeDataV1 = SnipeDataV1(0, false)
         val CODEC = RecordCodecBuilder.create<SnipeDataV1> { builder ->
             builder.group(
                 Codec.INT.fieldOf("ticks").forGetter { it.ticks },
@@ -169,7 +169,7 @@ data class SnipeDataV1(val ticks: Int, val loaded: Boolean) : SimpleStorageCompo
 
 data class CapacitanceDataV1(val damage: Float) : SimpleStorageComponent {
     companion object {
-        fun default(): CapacitanceDataV1 = CapacitanceDataV1(0f)
+        val DEFAULT: CapacitanceDataV1 = CapacitanceDataV1(0f)
         val CODEC = RecordCodecBuilder.create<CapacitanceDataV1> { builder ->
             builder.group(Codec.FLOAT.fieldOf("ticks").forGetter { it.damage })
                 .apply(builder, ::CapacitanceDataV1)
@@ -182,7 +182,7 @@ data class CapacitanceDataV2(
     val countdownTime: Int,
 ) : SimpleStorageComponent {
     companion object {
-        fun default(): CapacitanceDataV2 = CapacitanceDataV2(0, 0)
+        val DEFAULT: CapacitanceDataV2 = CapacitanceDataV2(0, 0)
         val CODEC = Codecs.NONNEGATIVE_INT.listOf().xmap(
             { list -> CapacitanceDataV2(list[0], list[1]) },
             { data -> listOf(data.dischargeTime, data.countdownTime) }
@@ -192,7 +192,7 @@ data class CapacitanceDataV2(
 
 data class TotemData(val target: UUID?) : SimpleStorageComponent {
     companion object {
-        fun default(): TotemData = TotemData(null)
+        val DEFAULT: TotemData = TotemData(null)
         val CODEC = Codecs.ESCAPED_STRING.xmap(
             { string -> TotemData(if (string.isEmpty()) null else UUID.fromString(string)) },
             { data -> data.target.toString() }
