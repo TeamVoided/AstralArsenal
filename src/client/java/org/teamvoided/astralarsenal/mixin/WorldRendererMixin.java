f0e18424cfd789fc12999f454edcbe5903cadf50
TheEnderCore <theendercore@gmail.com>
2025-02-01 18:37:02 +0200

    - moved Helper to misc

--- src/client/java/org/teamvoided/astralarsenal/mixin/WorldRendererMixin.java
+++ src/client/java/org/teamvoided/astralarsenal/mixin/WorldRendererMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static org.teamvoided.astralarsenal.Utils_HelperKt.getShapeAndPos;
+import static org.teamvoided.astralarsenal.utils.HelperKt.getShapeAndPos;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
