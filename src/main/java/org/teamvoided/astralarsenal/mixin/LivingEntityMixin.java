23bb72436355f66a7f2826c9bc023cd3a08d12e4
TheEnderCore <theendercore@gmail.com>
2025-03-08 23:26:02 +0200

    - forgor

--- src/main/java/org/teamvoided/astralarsenal/mixin/LivingEntityMixin.java
+++ src/main/java/org/teamvoided/astralarsenal/mixin/LivingEntityMixin.java
@@ -14,7 +14,6 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
