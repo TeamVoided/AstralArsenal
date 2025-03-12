6d71094cadc1c75940dc051e166d83341de8f395
TheEnderCore <theendercore@gmail.com>
2025-03-12 15:54:17 +0200

    - KnockbackExplosionBehavior

--- src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/KnockbackExplosionBehavior.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/KnockbackExplosionBehavior.kt
@@ -1,44 +1,16 @@
 package org.teamvoided.astralarsenal.world.explosion
 
-import net.minecraft.block.BlockState
 import net.minecraft.entity.Entity
-import net.minecraft.entity.damage.DamageSource
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.BlockView
 import net.minecraft.world.explosion.Explosion
-import net.minecraft.world.explosion.ExplosionBehavior
 import org.teamvoided.astralarsenal.init.AstralDamageTypes
+import org.teamvoided.astralarsenal.init.AstralDamageTypes.customDamage
 
-class KnockbackExplosionBehavior(causingEntity: Entity?) : ExplosionBehavior() {
-    val cause = causingEntity
-    override fun canDestroyBlock(
-        explosion: Explosion,
-        world: BlockView,
-        pos: BlockPos,
-        state: BlockState,
-        power: Float
-    ): Boolean {
-        return false
-    }
+class KnockbackExplosionBehavior(owner: Entity) : OwnedExplosionBehavior(owner) {
+    override fun shouldDamage(explosion: Explosion, entity: Entity): Boolean = owner == entity
+    override fun getKnockbackMultiplier(target: Entity): Float = 2.5f
 
-    override fun shouldDamage(explosion: Explosion, entity: Entity): Boolean {
-        return cause == entity
-    }
-
-    override fun getKnockbackMultiplier(target: Entity): Float {
-        return 2.5f
-    }
-
-    override fun calculateDamage(explosion: Explosion, entity: Entity): Float {
-        if (cause == entity) {
-            entity.damage(
-                DamageSource(
-                    AstralDamageTypes.getHolder(entity.world.registryManager, AstralDamageTypes.BOOM),
-                    cause,
-                    cause
-                ), 5f
-            )
-        }
+    override fun calculateDamage(explosion: Explosion?, entity: Entity?): Float {
+        if (owner == entity) entity.customDamage(AstralDamageTypes.BOOM, 5f, owner, owner)
         return 0f
     }
 }
