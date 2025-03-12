b3d6498c140d8e1db2ebbb95ce93195d84bac09b
TheEnderCore <theendercore@gmail.com>
2025-03-12 15:57:57 +0200

    - WeakExplosionBehavior

--- src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/WeakExplosionBehavior.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/WeakExplosionBehavior.kt
@@ -1,43 +1,16 @@
 package org.teamvoided.astralarsenal.world.explosion
 
-import net.minecraft.block.BlockState
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
-import net.minecraft.entity.damage.DamageSource
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.BlockView
 import net.minecraft.world.explosion.Explosion
-import net.minecraft.world.explosion.ExplosionBehavior
 import org.teamvoided.astralarsenal.init.AstralDamageTypes
+import org.teamvoided.astralarsenal.init.AstralDamageTypes.customDamage
 
-class WeakExplosionBehavior(causingEntity: Entity) : ExplosionBehavior() {
-    val causingEntity = causingEntity
-    override fun canDestroyBlock(
-        explosion: Explosion,
-        world: BlockView,
-        pos: BlockPos,
-        state: BlockState,
-        power: Float
-    ): Boolean {
-        return false
-    }
-
-    override fun getKnockbackMultiplier(target: Entity): Float {
-        return 2.5f
-    }
+class WeakExplosionBehavior(owner: Entity) : OwnedExplosionBehavior(owner) {
+    override fun getKnockbackMultiplier(target: Entity): Float = 2.5f
 
     override fun calculateDamage(explosion: Explosion?, entity: Entity?): Float {
-        if (explosion != null) {
-            if (entity is LivingEntity) {
-                entity.damage(
-                    DamageSource(
-                        AstralDamageTypes.getHolder(entity.world.registryManager, AstralDamageTypes.BOOM),
-                        causingEntity,
-                        causingEntity
-                    ), 10f
-                )
-            }
-        }
+        if (entity is LivingEntity) entity.customDamage(AstralDamageTypes.BOOM, 10f, owner, owner)
         return 0f
     }
 }
