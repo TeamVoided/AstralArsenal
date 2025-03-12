95332b9e47b8f23b0c800a2f3fc6f26b7422a983
TheEnderCore <theendercore@gmail.com>
2025-03-12 15:51:44 +0200

    - RancidExplosionBehavior

--- src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/RancidExplosionBehavior.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/RancidExplosionBehavior.kt
@@ -1,70 +1,29 @@
 package org.teamvoided.astralarsenal.world.explosion
 
-import net.minecraft.block.BlockState
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.BlockView
+import net.minecraft.registry.Holder
 import net.minecraft.world.explosion.Explosion
-import net.minecraft.world.explosion.ExplosionBehavior
 import org.teamvoided.astralarsenal.init.AstralEffects
 
-class RancidExplosionBehavior : ExplosionBehavior() {
-
-    override fun canDestroyBlock(
-        explosion: Explosion,
-        world: BlockView,
-        pos: BlockPos,
-        state: BlockState,
-        power: Float
-    ): Boolean {
-        return false
-    }
-
-    override fun getKnockbackMultiplier(target: Entity): Float {
-        return 2.5f
-    }
-
-    override fun calculateDamage(explosion: Explosion, entity: Entity): Float {
+class RancidExplosionBehavior : BlockSafeExplosionBehavior() {
+    override fun getKnockbackMultiplier(target: Entity): Float = 2.5f
+    override fun calculateDamage(explosion: Explosion?, entity: Entity?): Float {
         if (entity is LivingEntity) {
-            entity.addStatusEffect(
-                StatusEffectInstance(
-                    StatusEffects.SLOWNESS,
-                    600, 0,
-                    false, true, true
-                )
-            )
-            entity.addStatusEffect(
-                StatusEffectInstance(
-                    StatusEffects.POISON,
-                    600, 0,
-                    false, true, true
-                )
-            )
-            entity.addStatusEffect(
-                StatusEffectInstance(
-                    StatusEffects.WEAKNESS,
-                    600, 0,
-                    false, true, true
-                )
-            )
-            entity.addStatusEffect(
-                StatusEffectInstance(
-                    AstralEffects.REDUCE,
-                    600, 0,
-                    false, true, true
-                )
-            )
-            entity.addStatusEffect(
-                StatusEffectInstance(
-                    AstralEffects.BLEED,
-                    600, 0,
-                    false, true, true
-                )
-            )
+            entity.addEffect(StatusEffects.SLOWNESS)
+            entity.addEffect(StatusEffects.POISON)
+            entity.addEffect(StatusEffects.WEAKNESS)
+            entity.addEffect(AstralEffects.REDUCE)
+            entity.addEffect(AstralEffects.BLEED)
         }
         return 0f
     }
+
+    fun LivingEntity.addEffect(type: Holder<StatusEffect>, duration: Int = 600, amplifier: Int = 0) =
+        this.addStatusEffect(
+            StatusEffectInstance(type, duration, amplifier, false, true, true)
+        )
 }
