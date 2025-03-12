38c432bf9d307f750e90b4a2df68df317bf82c2d
TheEnderCore <theendercore@gmail.com>
2025-03-12 16:04:08 +0200

    - PenopticonExplosionBehavior

--- src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/PenopticonExplosionBehavior.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/PenopticonExplosionBehavior.kt
@@ -1,52 +1,18 @@
 package org.teamvoided.astralarsenal.world.explosion
 
-import net.minecraft.block.BlockState
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
-import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.BlockView
 import net.minecraft.world.explosion.Explosion
-import net.minecraft.world.explosion.ExplosionBehavior
 import org.teamvoided.astralarsenal.init.AstralDamageTypes
+import org.teamvoided.astralarsenal.init.AstralDamageTypes.customDamage
 
-class PenopticonExplosionBehavior(val causingEntity: Entity) : ExplosionBehavior() {
-
-    override fun canDestroyBlock(
-        explosion: Explosion,
-        world: BlockView,
-        pos: BlockPos,
-        state: BlockState,
-        power: Float
-    ): Boolean {
-        return false
-    }
-
-    override fun getKnockbackMultiplier(target: Entity): Float {
-        return 5f
-    }
+class PenopticonExplosionBehavior(owner: Entity) : OwnedExplosionBehavior(owner) {
+    override fun getKnockbackMultiplier(target: Entity): Float = 5f
 
     override fun calculateDamage(explosion: Explosion?, entity: Entity?): Float {
-        if (explosion != null) {
-            if (entity is PlayerEntity) {
-                entity.damage(
-                    DamageSource(
-                        AstralDamageTypes.getHolder(entity.world.registryManager, AstralDamageTypes.BOOM),
-                        causingEntity,
-                        causingEntity
-                    ), 25f
-                )
-            } else if (entity is LivingEntity) {
-                entity.damage(
-                    DamageSource(
-                        AstralDamageTypes.getHolder(entity.world.registryManager, AstralDamageTypes.BOOM),
-                        causingEntity,
-                        causingEntity
-                    ), 50f
-                )
-            }
-        }
+        if (entity is LivingEntity)
+            entity.customDamage(AstralDamageTypes.BOOM, if (entity is PlayerEntity) 25f else 50f, owner, owner)
         return 0f
     }
 }
