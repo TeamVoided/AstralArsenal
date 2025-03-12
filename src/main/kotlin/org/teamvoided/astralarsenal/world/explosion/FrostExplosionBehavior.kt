7f12d16be62f5efd09f7e96f6a8bde746e6d77e6
TheEnderCore <theendercore@gmail.com>
2025-03-12 15:51:28 +0200

    - FrostExplosionBehavior

--- src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/FrostExplosionBehavior.kt
+++ src/main/kotlin/org/teamvoided/astralarsenal/world/explosion/FrostExplosionBehavior.kt
@@ -1,30 +1,12 @@
 package org.teamvoided.astralarsenal.world.explosion
 
-import net.minecraft.block.BlockState
 import net.minecraft.entity.Entity
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.BlockView
 import net.minecraft.world.explosion.Explosion
-import net.minecraft.world.explosion.ExplosionBehavior
 
-class FrostExplosionBehavior : ExplosionBehavior() {
-
-    override fun canDestroyBlock(
-        explosion: Explosion,
-        world: BlockView,
-        pos: BlockPos,
-        state: BlockState,
-        power: Float
-    ): Boolean {
-        return false
-    }
-
-    override fun getKnockbackMultiplier(target: Entity): Float {
-        return 2.5f
-    }
-
-    override fun calculateDamage(explosion: Explosion, entity: Entity): Float {
-        entity.frozenTicks += 1000
+class FrostExplosionBehavior : BlockSafeExplosionBehavior() {
+    override fun getKnockbackMultiplier(target: Entity): Float = 2.5f
+    override fun calculateDamage(explosion: Explosion?, entity: Entity?): Float {
+        entity?.let { it.frozenTicks += 1000 }
         return 0f
     }
 }
