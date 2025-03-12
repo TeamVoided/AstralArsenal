759a900184c8c47360941ce5007d44aeecc60ac7
TheEnderCore <theendercore@gmail.com>
2025-03-12 14:59:39 +0200

    - custom breaking animation for custom outline tools

--- src/client/java/org/teamvoided/astralarsenal/mixin/WorldRendererMixin.java
+++ src/client/java/org/teamvoided/astralarsenal/mixin/WorldRendererMixin.java
@@ -1,13 +1,15 @@
 package org.teamvoided.astralarsenal.mixin;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,7 +17,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static org.teamvoided.astralarsenal.utils.HelperKt.getShapeAndPos;
+import java.util.SortedSet;
+
+import static org.teamvoided.astralarsenal.utils.OutlineHelperKt.*;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
@@ -24,29 +28,32 @@ public abstract class WorldRendererMixin {
     @Final
     private MinecraftClient client;
 
-
     @Shadow
     private static void drawShapeOutline(MatrixStack matrices, VertexConsumer consumer, VoxelShape voxelShape, double x, double y, double z, float red, float green, float blue, float alpha) {
     }
 
+    @Shadow
+    @Final
+    private Long2ObjectMap<SortedSet<BlockBreakingInfo>> blockBreakingProgressions;
+
     @Inject(method = "drawBlockOutline", at = @At("HEAD"), cancellable = true)
     private void astralArsenal$hammerOutline(MatrixStack matrices, VertexConsumer consumer, Entity entity, double offsetX, double offsetY, double offsetZ, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
-        var shapesAndPos = getShapeAndPos(client);
+        var shapesAndPos = getOutlineShape(client);
         if (shapesAndPos == null) return;
 
         var pos = shapesAndPos.getSecond();
         drawShapeOutline(
-                matrices,
-                consumer,
-                shapesAndPos.getFirst(),
-                pos.getX() - offsetX,
-                pos.getY() - offsetY,
-                pos.getZ() - offsetZ,
-                0.0F,
-                0.0F,
-                0.0F,
-                0.4F);
+                matrices, consumer, shapesAndPos.getFirst(),
+                pos.getX() - offsetX, pos.getY() - offsetY, pos.getZ() - offsetZ,
+                0.0F, 0.0F, 0.0F, 0.4F
+        );
         ci.cancel();
     }
 
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;getModelViewStack()Lorg/joml/Matrix4fStack;", remap = false))
+    private void modifyBreakingInfoStack(DeltaTracker tracker, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f modelViewMatrix, Matrix4f projectionMatrix, CallbackInfo ci) {
+        var positions = getPositions(client);
+        if (positions.size() < 2) return;
+        addExtraBreakingInfo(client, positions, this.blockBreakingProgressions);
+    }
 }
--- src/client/kotlin/org/teamvoided/astralarsenal/utils/OutlineHelper.kt
+++ src/client/kotlin/org/teamvoided/astralarsenal/utils/OutlineHelper.kt
@@ -1,8 +1,10 @@
 package org.teamvoided.astralarsenal.utils
 
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap
 import net.minecraft.block.BlockState
 import net.minecraft.block.ShapeContext
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.render.BlockBreakingInfo
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.ItemUsageContext
@@ -21,49 +23,74 @@ import org.teamvoided.astralarsenal.kosmogliph.Kosmogliph
 import org.teamvoided.astralarsenal.kosmogliph.SimpleKosmogliph
 import org.teamvoided.astralarsenal.kosmogliph.logic.*
 import org.teamvoided.astralarsenal.util.getKosmogliphs
+import java.util.*
 import kotlin.math.min
 
 
 // this code is a modified version fabric-hammers mod
 // https://github.com/bdani0717/fabric-hammers-1.20
-fun getShapeAndPos(client: MinecraftClient): Pair<VoxelShape, BlockPos>? {
-    val player = client.player ?: return null
-    val world = client.world ?: return null
+fun getPositions(client: MinecraftClient): Set<BlockPos> {
+    val player = client.player ?: return setOf()
+    val world = client.world ?: return setOf()
 
-    if (player.isSneaking) return null
+    if (player.isSneaking) return setOf()
 
     val stack = player.mainHandStack
     val comp = stack.getKosmogliphs()
-    if (comp.isEmpty()) return null
+    if (comp.isEmpty()) return setOf()
 
-    val glyph = comp.outShapeChanger() ?: return null
+    val glyph = comp.changesMiningShape() ?: return setOf()
 
+    if (client.crosshairTarget !is BlockHitResult) return setOf()
 
-    if (client.crosshairTarget is BlockHitResult) {
-        val pos: BlockPos = (client.crosshairTarget as BlockHitResult).blockPos
-        val state = client.world!!.getBlockState(pos)
+    val pos: BlockPos = (client.crosshairTarget as BlockHitResult).blockPos
+    val state = client.world!!.getBlockState(pos)
+    if (!state.isAir && client.world!!.worldBorder.contains(pos)) return glyph.getBlocks(
+        stack,
+        world,
+        pos,
+        state,
+        player
+    )
 
-        if (!state.isAir && client.world!!.worldBorder.contains(pos)) {
-            val positions = glyph.getBlocks(stack, world, pos, state, player)
-            if (positions.isEmpty()) return null
+    return setOf()
+}
 
-            var outlineShape = VoxelShapes.empty()
-            for (position in positions) {
-                val diffPos = position.subtract(pos)
-                val offsetBlock = world.getBlockState(position)
-                if (!offsetBlock.isAir) outlineShape = VoxelShapes.union(
-                    outlineShape, offsetBlock.getOutlineShape(world, position, ShapeContext.of(player))
-                        .offset(diffPos.x.toDouble(), diffPos.y.toDouble(), diffPos.z.toDouble())
-                )
-            }
-            return outlineShape to pos
-        }
+fun getOutlineShape(client: MinecraftClient): Pair<VoxelShape, BlockPos>? {
+    val positions = getPositions(client)
+    if (positions.size < 2) return null
+    val world = client.world ?: return null
+    val player = client.player ?: return null
+    val pos: BlockPos = (client.crosshairTarget as BlockHitResult).blockPos
+
+    var outlineShape = VoxelShapes.empty()
+    for (position in positions) {
+        val diffPos = position.subtract(pos)
+        val offsetBlock = world.getBlockState(position)
+        if (!offsetBlock.isAir) outlineShape = VoxelShapes.union(
+            outlineShape,
+            offsetBlock.getOutlineShape(world, position, ShapeContext.of(player))
+                .offset(diffPos.x.toDouble(), diffPos.y.toDouble(), diffPos.z.toDouble())
+        )
     }
-    return null
+    return outlineShape to pos
+}
+
+fun addExtraBreakingInfo(
+    client: MinecraftClient, positions: MutableSet<BlockPos>,
+    blockBreakingProgressions: Long2ObjectMap<SortedSet<BlockBreakingInfo>>
+) {
+    if (client.crosshairTarget !is BlockHitResult) return
+    val mainPos = (client.crosshairTarget as BlockHitResult).blockPos
+    val breakingPos = positions.filter { it != mainPos }
+    val info = blockBreakingProgressions.get(mainPos.asLong())
+    if (info == null || info.isEmpty()) return
+
+    for (pos in breakingPos) blockBreakingProgressions.put(pos.asLong(), info)
 }
 
 
-fun KosmogliphsComponent.outShapeChanger(): SimpleKosmogliph? = if (this.contains(HAMMER)) HAMMER
+fun KosmogliphsComponent.changesMiningShape(): SimpleKosmogliph? = if (this.contains(HAMMER)) HAMMER
 else if (this.contains(REAPER)) REAPER
 else if (this.contains(VEIN_MINER)) VEIN_MINER
 else null
@@ -94,16 +121,11 @@ fun queryReaperTillable(pos: BlockPos, world: World, miner: PlayerEntity): Set<B
     val castEnd = miner.eyePos.add(miner.rotationVector.multiply(miner.reach()))
     val raycast = world.raycast(
         RaycastContext(
-            miner.eyePos,
-            castEnd,
-            RaycastContext.ShapeType.OUTLINE,
-            RaycastContext.FluidHandling.NONE,
-            miner
+            miner.eyePos, castEnd, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, miner
         )
     )
 
-    return areaOfAffect(pos, raycast.side).allInside()
-        .filter {
+    return areaOfAffect(pos, raycast.side).allInside().filter {
             if (!it.isInWorld(world)) return@filter false
             val predicate = TillingActions.get[world.getBlockState(it).block] ?: return@filter false
             predicate.first.test(ItemUsageContext(miner, miner.activeHand, raycast))
